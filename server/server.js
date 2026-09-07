84145a0560b02363a9c350459cdde98fe4b20103
Hari Shree <harishreep07@gmail.com>
2026-09-07 19:22:07 +0530

    Allow deployed frontend to access API

--- server/server.js
+++ server/server.js
@@ -9,8 +9,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(
+app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://resume-generator-five-iota.vercel.app",
+    ],
   })
 );
 
